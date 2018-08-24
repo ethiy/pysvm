650b561bd4120e32ddea1b767c896fb618c28083
Oussama ENNAFII <oussama.ennafii@polytechnique.org>
2018-08-24 14:59:11 +0200

    setup + requirements

---
+++ setup.py
@@ -0,0 +1,53 @@
+#! /usr/bin/env python3
+# -*- coding: <utf-8> -*-
+
+from setuptools import setup, find_packages
+from qualcity import config
+
+def readme():
+    """
+    Longer description from readme.
+    """
+    with open('README.md', 'r') as readmefile:
+        return readmefile.read()
+
+
+def requirements():
+    """
+    Get requirements to install.
+    """
+    with open('requirements.txt', 'r') as requirefile:
+        return [line.strip() for line in requirefile.readlines() if line]
+
+
+setup(
+    name='pysvm',
+    version=config.__version__,
+    description='Python SVM SMO',
+    long_description=readme(),
+    classifiers=[
+        'License :: MIT License',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Research',
+        'Topic :: Scientific/Engineering',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    platforms=[
+        'Environment :: Console',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Windows'
+    ],
+    scripts=['qual-city'],
+    keywords='Machine Learning SVM SMO',
+    url='https://github.com/ethiy/pysvm',
+    author='Oussama Ennafii',
+    author_email='oussama.ennafii@outlook.fr',
+    license='License :: OSI Approved :: MIT License',
+    packages=find_packages(exclude=['tests']),
+    install_requires=requirements(),
+    include_package_data=True,
+    zip_safe=False
+)
