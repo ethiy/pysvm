9c5f7e65f3344db685a6445720951b63fc919864
Oussama ENNAFII <oussama.ennafii@polytechnique.org>
2018-08-25 12:44:45 +0200

    ALL

---
+++ pysvm/__init__.py
@@ -0,0 +1,11 @@
+from svm import BinarySVM
+from svm import OnevsAllSVM
+from svm import OnevsOneSVM
+from svm import SVM
+
+__all__ = [
+    "BinarySVM",
+    "OnevsAllSVM",
+    "OnevsOneSVM",
+    "SVM"
+]
