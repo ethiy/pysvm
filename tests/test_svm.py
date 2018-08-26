fef36dd570c09f0917b42efb60a6421dac455a72
Oussama ENNAFII <oussama.ennafii@polytechnique.org>
2018-08-26 17:15:23 +0100

    tests

--- tests/test_svm.py
+++ tests/test_svm.py
@@ -8,21 +8,17 @@ import unittest
 import sklearn.datasets
 import numpy as np
 
+import matplotlib.pyplot as plt
 
 def generate_guassian(mean, covar, samples):
     return np.random.multivariate_normal(mean, covar, samples)
 
 
-def generate_real_separator():
-    w, b = np.random.rand(2, 1), np.random.rand(1)
-    w = w / np.linalg.norm(w)
-    return (w, b)
-
-def linearly_separable_dataset(w, b):
+def synthetic_separable():
     return np.vstack(
         (
-            generate_guassian([3, 0], [[0, 10], [0.1, 10]], 500),
-            generate_guassian([-3, 0], [[0, 10], [0.1, 10]], 500)
+            generate_guassian([3, 0], [[0.1, 0.1], [0.1, 10]], 500),
+            generate_guassian([-3, 0], [[0.1, 0.1], [0.1, 10]], 500)
         )
     ), np.concatenate(
         (
@@ -37,13 +33,18 @@ class BinaryTest(unittest.TestCase):
     Test case for the 'BinarySVM' 'svm' class.
     """
 
-    def test_synthetic(self):
-        # w, b = generate_real_separator()
-        X, y = linearly_separable_dataset(np.array([0, 1]), 0)
-        model = BinarySVM(max_iter=1000, C=.01)
+    def test_synthetic_separable(self):
+        X, y = synthetic_separable()
+        model = BinarySVM(max_iter=1000, epsilon=1E-15, C=10)
         model.fit(X, y)
-        print(model.w/ np.linalg.norm(model.w), model.b, model.error)
+        self.assertLessEqual(abs(model.w[1] / np.linalg.norm(model.w)), .05)
 
+    
+    def test_synthetic_cocentric(self):
+        X, y = sklearn.datasets.make_circles(n_samples=1000, factor=.3, noise=.05)
+        model = BinarySVM(max_iter=300, epsilon=1E-15, C=2, kernel=lambda x, y: x.dot(y)**4)
+        model.fit(X, 2*y-1)
+        self.assertLessEqual(abs(model.b), .01)
 
 def main():
     unittest.main()
