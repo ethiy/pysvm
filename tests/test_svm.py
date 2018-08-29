2c269b39e6157646d70d815f46b30b75a10ec9a4
Oussama ENNAFII <oussama.ennafii@polytechnique.org>
2018-08-29 22:09:49 +0100

    tests

--- tests/test_svm.py
+++ tests/test_svm.py
@@ -31,29 +31,19 @@ class BinaryTest(unittest.TestCase):
     Test case for the 'BinarySVM' 'svm' class.
     """
 
-    # def test_synthetic_separable(self):
-    #     X, y = synthetic_separable()
-    #     model = BinarySVM(max_iter=20, C=1, debug=True, verbose=True)
-    #     model.fit(X, y)
-    #     plt.plot(model.lds)
-    #     plt.show()
-    #     w = model.w()
-    #     print(w/np.linalg.norm(w))
-    #     print(np.argwhere(np.isnan(model.K)).shape)
-    #     print(model.b_up,model.b_low)
-        # self.assertLessEqual(abs(w[1] / np.linalg.norm(w)), .01)
+    def test_synthetic_separable(self):
+        X, y = synthetic_separable()
+        model = BinarySVM(max_iter=100, C=100)
+        model.fit(X, y)
+        self.assertLessEqual(abs(w[1] / np.linalg.norm(w)), .01)
 
     
     def test_synthetic_cocentric(self):
         X, y = sklearn.datasets.make_circles(n_samples=1000, factor=.3, noise=.05)
-        model = BinarySVM(max_iter=100, C=10, debug=True, verbose=True, kernel=lambda x, y: x.dot(y.T)**2)
+        model = BinarySVM(max_iter=100, C=100, kernel=lambda x, y: x.dot(y.T)**2)
         model.fit([np.array(x) for x in X], (2*y-1).tolist())
-        plt.plot(model.lds)
-        plt.show()
         print(np.argwhere(np.isnan(model.K)).shape)
-        print(model.b_up,model.b_low)
-        # print([model.phi(model.X[s]) - model.Y[s] for s in model.support_vectors_idx])
-    #     # self.assertLessEqual(abs(model.b), .000001)
+        self.assertLess(abs(model.b + 1.8), .1)
 
 def main():
     unittest.main()
